e6684b8a162a9142277e52550afc62409d9e7940
Holome <70796817+Holome-1st@users.noreply.github.com>
2025-03-17 16:14:06 +0100

    1.1.1

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
 // ==UserScript==
-// @name          Twitch Auto Claim Bonus Points
-// @version       1.1.0
-// @description   Claim automatically bonus points in Twitch.
-// @icon          https://i.ibb.co/Y7mWWKGT/Twitch-Auto-Claim-Bonus-Points-Icon.png
-// @grant         none
-// @author        Holome
-// @homepage      https://github.com/Holome-1st/Twitch-Auto-Claim-Bonus-Points
-// @match         https://www.twitch.tv/*
-// @updateURL     https://raw.githubusercontent.com/Holome-1st/Twitch-Auto-Claim-Bonus-Points/refs/heads/main/main.js
-// @downloadURL   https://raw.githubusercontent.com/Holome-1st/Twitch-Auto-Claim-Bonus-Points/refs/heads/main/main.js
+// @name			Twitch Auto Claim Bonus Points
+// @version			1.1.1
+// @description		Claim automatically bonus points in Twitch.
+// @icon			https://i.ibb.co/Y7mWWKGT/Twitch-Auto-Claim-Bonus-Points-Icon.png
+// @grant			none
+// @author			Holome
+// @homepage		https://github.com/Holome-1st/Twitch-Auto-Claim-Bonus-Points
+// @match			https://www.twitch.tv/*
+// @updateURL		https://raw.githubusercontent.com/Holome-1st/Twitch-Auto-Claim-Bonus-Points/refs/heads/main/main.js
+// @downloadURL		https://raw.githubusercontent.com/Holome-1st/Twitch-Auto-Claim-Bonus-Points/refs/heads/main/main.js
 // ==/UserScript==
 
 (function() {
-    'use strict';
+	'use strict';
 
-    const observer = new MutationObserver(() => {
-        const buttonNodes = document.getElementsByTagName("button");
+	const observer = new MutationObserver(() => {
+		const buttonNodes = document.getElementsByTagName("button");
 
-        // Iterate over each button found.
-        Array.from(buttonNodes).forEach((buttonNode) => {
-            try {
-                // Checks if the button is eligible for claiming.
-                if (buttonNode.getElementsByClassName("claimable-bonus__icon").length < 1) return;
-                if (buttonNode.classList.contains("claimed")) return;
-                if (buttonNode.hasAttribute("disabled")) return;
+		// Iterate over each button found.
+		Array.from(buttonNodes).forEach((buttonNode) => {
+			try {
+				// Checks if the button is eligible for claiming.
+				if (buttonNode.getElementsByClassName("claimable-bonus__icon").length < 1) return;
+				if (buttonNode.classList.contains("claimed")) return;
+				if (buttonNode.hasAttribute("disabled")) return;
 
-                // Clicks the button and marks it as claimed.
-                buttonNode.click();
-                buttonNode.classList.add("claimed");
-                console.log("Bonus claimed.", buttonNode);
-            } catch (error) {
-                console.error("Unable to claim the bonus.", error);
-            }
-        });
-    });
+				// Clicks the button and marks it as claimed.
+				buttonNode.click();
+				buttonNode.classList.add("claimed");
+				console.log("Bonus claimed.", buttonNode);
+			} catch (error) {
+				console.error("Unable to claim the bonus.", error);
+			}
+		});
+	});
 
-    try {
-        // Start observing the body.
-        observer.observe(document.body, { childList: true, subtree: true });
-        console.log("Start observation...");
-    } catch (error) {
-        console.error("Unable to start the observation.", error);
-    }
+	try {
+		// Start observing the body.
+		observer.observe(document.body, { childList: true, subtree: true });
+		console.log("Start observation...");
+	} catch (error) {
+		console.error("Unable to start the observation.", error);
+	}
 })();
